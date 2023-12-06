17010f6d90aafa15297a84bc01ab6c1cba3dadcc
Bianca Marchan <bimarchan@cps.edu>
2023-12-06 11:16:57 -0600

    finshed

--- part1-linear-regression/example3_blood_pressure.py
+++ part1-linear-regression/example3_blood_pressure.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
 
 '''
 Run the program and consider the following questions:
@@ -14,13 +13,7 @@ x = data["Age"]
 y = data["Blood Pressure"]
 
 #sets the size of the graph
-
-x = x.reshape(-1, 1)
-
-
 plt.figure(figsize=(5,4))
-
-model = LinearRegression().fit(x, y)
 
 #labels axes and creates scatterplot
 plt.xlabel("Age")
@@ -29,40 +22,5 @@ plt.title("Systolic Blood Pressure by Age")
 plt.scatter(x, y)
 
 print("Pearson's Correlation: r = :", x.corr(y))
-x = x.reshape(-1, 1)
 
-# create the model
-model = LinearRegression().fit(x, y)
-
-# find the coefficient, bias, and r squared values
-# each should be a float and rounded to two decimal places
-coef = round(float(model.coef_), 2)
-intercept = round(float(model.intercept_), 2)
-r_squared = model.score(x, y)
-
-# value you are going to predict
-x_predict = 25
-# plug that value into your model
-prediction = model.predict([[x_predict]])
-
-# print out the linear equation and r squared value
-print(f"Model's Linear Equation: y = {coef}x + {intercept}")
-print(f"R Squared value: {r_squared}")
-print(f"Prediction when x is {x_predict}: {prediction}")
-
-'''
-The following code creates the graph to visualize the data
-'''
-
-# creates a scatter plot of originial data in purple
-# and the predicted data in blue
-plt.scatter(x,y, c="purple")
-plt.scatter(x_predict, prediction, c="blue")
-
-
-# plot the line of best fit in red and label the line
-plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
-
-# show the plot and legend
-plt.legend()
 plt.show()
