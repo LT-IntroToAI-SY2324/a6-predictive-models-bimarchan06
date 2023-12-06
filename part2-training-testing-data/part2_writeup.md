7ef01c7f8ed68f66ec1d613055107b94a352c34b
Bianca Marchan <bimarchan@cps.edu>
2023-12-06 11:16:43 -0600

    Finished A2 code

--- part2-training-testing-data/part2_writeup.md
+++ part2-training-testing-data/part2_writeup.md
@@ -5,9 +5,11 @@ After completing `a6_part2.py` answer the following questions
 ## Questions to answer
 
 1. What makes this model more effective than the model you created in the previous lesson?
-
+This model is more effective than the previous lesson because it has a larger scale of data. It allows for more items to be examined and that allows more ground to be covered.
 
 2. What does the R squared coefficient tell you about the model?
-
+The R square coefficient tells us how the ae of a person doesn't give a direct corelation/ effect with blood pressure.
 
 3. Would you say that your model is accurate? What evidence supports your conclusion? Consider the meaning of the predicted and actual values in the context of the chart below from the American Heart Association’s website on understanding blood pressure.
+
+The predicted and actual values vary but not too great. Some "normal" values elevated in comparasion to the actual normal levels. This can be dangerous for people to use because it will give data that isn't too accurate which can cause people to suffer from it because they will have bad blood pressure readings.  
--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -3,18 +3,52 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
+
+data = pd.read_csv("part3-multivariable-linear-regression/antelope_data.csv")
+x_1 = data[""]
+x_2 = data["Winter Severity"]
+y = data["Fawn"]
+
+fig, graph = plt.subplots(3)
+graph[0].scatter(x_1, y)
+graph[0].set_xlabel("Annual Precipitation")
+graph[0].set_ylabel("Fawn")
+
+graph[1].scatter(x_2, y)
+graph[1].set_xlabel("Winter Severity")
+graph[1].set_ylabel("Fawn")
+
+graph[2].scatter(x_3, y)
+graph[2].set_xlabel("Adult Population")
+graph[2].set_ylabel("Fawn")
+
+print("Correlation between Annual Precipitation and Fawn Population:",round(x_1.corr(y),2))
+print("Correlation between Winter Severity and Fawn Population:",round(x_2.corr(y),2))
+print("Correlation between Adult Population and Fawn Population:",round(x_3.corr(y),2))
+
+plt.tight_layout()
+plt.show()
+
 #imports and formats the data
 data = pd.read_csv("part3-multivariable-linear-regression/car_data.csv")
 x = data[["miles","age"]].values
 y = data["Price"].values
 
+
 #split the data into training and testing data
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
 
 #create linear regression model
+model = LinearRegression().fit(xtrain, ytrain)
 
 #Find and print the coefficients, intercept, and r squared values. 
 #Each should be rounded to two decimal places. 
+coef = np.around(model.coef_, 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = round(model.score(x, y),2)
 
+print(f"Model's Linear Equation: y={coef[0]}x1 + {coef[1]}x2 + {coef[2]}x3 + {intercept}")
+print("R Squared value:", r_squared)
 
 #Loop through the data and print out the predicted prices and the 
 #actual prices
