404958b8725cf85e333894717f19e8242caa420c
Bianca Marchan <bimarchan@cps.edu>
2023-12-07 11:18:11 -0600

    Finished A6 Part 3

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -3,37 +3,10 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-
-data = pd.read_csv("part3-multivariable-linear-regression/antelope_data.csv")
-x_1 = data[""]
-x_2 = data["Winter Severity"]
-y = data["Fawn"]
-
-fig, graph = plt.subplots(3)
-graph[0].scatter(x_1, y)
-graph[0].set_xlabel("Annual Precipitation")
-graph[0].set_ylabel("Fawn")
-
-graph[1].scatter(x_2, y)
-graph[1].set_xlabel("Winter Severity")
-graph[1].set_ylabel("Fawn")
-
-graph[2].scatter(x_3, y)
-graph[2].set_xlabel("Adult Population")
-graph[2].set_ylabel("Fawn")
-
-print("Correlation between Annual Precipitation and Fawn Population:",round(x_1.corr(y),2))
-print("Correlation between Winter Severity and Fawn Population:",round(x_2.corr(y),2))
-print("Correlation between Adult Population and Fawn Population:",round(x_3.corr(y),2))
-
-plt.tight_layout()
-plt.show()
-
 #imports and formats the data
 data = pd.read_csv("part3-multivariable-linear-regression/car_data.csv")
-x = data[["miles","age"]].values
+x = data[["miles(000)", "age"]].values
 y = data["Price"].values
-
 
 #split the data into training and testing data
 xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
@@ -47,10 +20,37 @@ coef = np.around(model.coef_, 2)
 intercept = round(float(model.intercept_), 2)
 r_squared = round(model.score(x, y),2)
 
-print(f"Model's Linear Equation: y={coef[0]}x1 + {coef[1]}x2 + {coef[2]}x3 + {intercept}")
+# print out the linear equation and r^2 value
+print(f"Model's Linear Equation: y={coef[0]}x1 + {coef[1]}x2 + {intercept}")
 print("R Squared value:", r_squared)
 
 #Loop through the data and print out the predicted prices and the 
 #actual prices
 print("***************")
 print("Testing Results")
+# get the predicted y values for the xtest values - returns an array of the results
+predict = model.predict(xtest)
+
+#Testing the writeup questions:
+
+carOne = model.predict([[89, 10]])
+print("A 10 year old car with 89,000 miles is worth $", carOne)
+
+carTwo = model.predict([[150, 20]])
+print("A 20 year old car with 150,000 miles is worth $", carTwo)
+
+carThree = model.predict([[500, 50]])
+print("Car three is worth $", carThree)
+
+# round the value in the np array to 2 decimal places
+predict = np.around(predict, 2)
+print(predict)
+
+# compare the actual and predicted values
+print("\nTesting Multivariable Model with Testing Data:")
+for index in range(len(xtest)):
+    actual = ytest[index] # gets the actual y value from the ytest dataset
+    predicted_y = predict[index] # gets the predicted y value from the predict variable
+    x_coord = xtest[index] # gets the x value from the xtest dataset
+    x_coord = np.around(x_coord, 2)
+    print(f"Miles: {x_coord[0]} Age: {x_coord[1]} Actual: {actual} Predicted: {predicted_y}")
